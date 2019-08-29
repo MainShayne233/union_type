516488d799a09e28d97475fb0b0b508da8449b40
MainShayne233 <shaynetremblay@gmail.com>
2019-08-29 13:30:45 -0400

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -31,6 +31,8 @@ defmodule UnionType.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
